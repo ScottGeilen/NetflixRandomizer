130c57ac9c8d3b15ace4ba3b2bea8a979f852afc
Scott <scottgeilen99@gmail.com>
2020-08-30 12:50:47 -0400

    v1.1

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
+
 import requests
 import random
 from bs4 import BeautifulSoup
 import pandas as pd
+
 page = requests.get('https://reelgood.com/source/netflix')
 soup = BeautifulSoup(page.text, 'html.parser')
 
@@ -15,5 +17,5 @@ for row in rows:
     titles = row.find_all('a')
     titles = [x.text.strip() for x in titles]
     title_list.append(titles[1])
-    #print(title_list)
+    
 print('You should consider watching ' + random.choice(title_list) + '!')
