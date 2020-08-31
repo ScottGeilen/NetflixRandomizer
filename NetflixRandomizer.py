6e1ce31e02366b86bb3f4089492c24838bb5c4bf
Scott <scottgeilen99@gmail.com>
2020-08-30 22:17:20 -0400

    v3 - Suggestion loop

--- NetflixRandomizer.py
+++ NetflixRandomizer.py
@@ -5,20 +5,31 @@ import requests
 import random
 from bs4 import BeautifulSoup
 import pandas as pd
+import tkinter as Tk
 
 page = requests.get('https://en.wikipedia.org/wiki/List_of_Netflix_original_programming')
 soup = BeautifulSoup(page.text, 'html.parser')
 
-title_list = []
-table = soup.find('div', {'id': 'mw-content-text'})
-body = table.find_all('tbody')
-rows = table.find_all('tr')
+print('\nNetflix Movie or Show Suggestions\n')
+exit = False
+suggestion = 'y'
+while exit == False:
+    if (suggestion.lower() == 'y'):
+        title_list = []
+        table = soup.find('div', {'id': 'mw-content-text'})
+        #body = table.find_all('tbody')
+        rows = table.find_all('tr')
 
-for row in rows:
-    title_row = row.find_all('td')
-    titles = [x.text.strip() for x in title_row]
-    title = titles[:1]
-    title_list.append(title)
-    
-choice = random.choice(title_list)
-print('You should consider watching ' + str(choice[0]) + '!')
+        for row in rows:
+            title_row = row.find_all('a')
+            titles = [x.text.strip() for x in title_row]
+            #title = titles[0]
+            title_list.append(titles)
+        choice = random.choice(title_list)
+        print('SUGGESTION: You should consider watching ' + str(choice[0]) + '!\n')
+        print('Need another suggestion? y/n')
+        suggestion = input()
+    elif (suggestion.lower() == 'n'):
+        print('Enjoy your show or movie!')
+        exit = True
+        break
